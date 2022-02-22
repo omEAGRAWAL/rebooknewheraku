8c0285cf3f361cbe5737e04256b64f671010b578
Vyasdev <65308856+Vyasdev217@users.noreply.github.com>
2022-02-22 20:06:24 +0530

    Create be.php

---
+++ be.php
@@ -0,0 +1,57 @@
+# backend_main
+# Created on : 2022 Feb 22
+
+# Change log YYYY/MM/DD : Contributor : Changes details
+# 2022/02/22 : Vyasdev : Created the file
+
+<?php
+
+@require "be_dependencies.php";
+
+function new_request(data){
+  
+}
+
+function edit_request(data){
+  
+}
+
+function get_request_list(data){
+  
+}
+
+function read_request(data){
+  
+}
+
+function new_campeign(data){
+  
+}
+
+function edit_campeign(data){
+  
+}
+
+function get_campeign_list(data){
+  
+}
+
+function read_campeign(data){
+  
+}
+
+function new_user(){
+  
+}
+
+function edit_user(){
+  
+}
+
+function get_user_list(){
+  
+}
+
+function list_user(){
+  
+}
