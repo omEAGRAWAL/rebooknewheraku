318b863a5e45c97550f4fb6959c9f08ac5f60045
Dharun2812 <97323354+Dharun2812@users.noreply.github.com>
2022-02-22 22:15:48 +0530

    Add files via upload

---
+++ rebook.js
@@ -0,0 +1,12 @@
+let nam=document.querySelector('button');
+nam.addEventListener('click',show);
+function show(){
+    var mode=prompt("time")
+ if(mode>5&& mode <17){
+     alert("good m")
+ }
+ else if 
+ else{
+    alert('eve');
+ }
+}
