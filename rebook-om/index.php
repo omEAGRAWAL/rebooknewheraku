abc7f0a74e079a642a65f172280dae9b032100dd
Dharun2812 <97323354+Dharun2812@users.noreply.github.com>
2022-02-22 20:24:15 +0530

    Add files via upload

---
+++ rebook-om/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="rebookstyle.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
+    <title>ReBook</title>
+  </head>
+
+  <body>
+
+    <div id="header_content" class="header">
+
+      <nav>
+
+        <a class="active" href="index.php"><img style="width: 20px;" src="home.png" alt=""></a>
+      
+        <a style="float:right; cursor:pointer"  type="a" class="login" data-toggle="modal" data-target="#myModal">
+            LogIn
+        </a>
+        <a class="request" href="request.php">Request</a>
+        <a class="donate" href="donate.php">Donate</a>
+      
+
+      </nav>
+    </div>
+
+    <div class="st1part">
+      <p id="1stline">  more than 30,00,000 orphans in india </p>
+      <p id="2ndline">your one book will help three students</p>
+      <p id="3rdline"> sharing is carrying</p>
+      <p id="4thline">join with us to bridge this gap</p>
+    </div>
+
+
+
+
+    <a href="camp1.html">
+      <div id="campaign1">
+        <img  id="pic1" src="img1.jpg" alt="">
+        <p>donate your childs book to them</p>
+        <a href="donatebook.html">
+          <button id="donate-button">donate now</button>
+        </a>
+        <p id="elementimg1"></p>
+        <p id="camp1"></p>
+
+        <p id="om"></p>
+
+      </div>
+    </a>
+    <footer>
+    
+      <div id="contact_us" class="contact">
+        <h2>Contact Us</h2>
+        <a href="mailto:dharunramsarathy@gmail.com">Mail address</a>
+      </div>
+    </footer> 
+    <?php require 'validate.php' ?>
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+          <div class="modal-content clearfix">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+              <div class="modal-body">
+                  <h3 class="title">Login Form</h3>
+                  <p class="description">Login here Using Email & Password</p>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                  <div class="form-group">
+                      <span class="input-icon"><i class="fa fa-user"></i></span>
+                      <input type="email" class="form-control" placeholder="Enter email" name="email">
+                  </div>
+                  <div class="form-group">
+                      <span class="input-icon"><i class="fas fa-key"></i></span>
+                      <input type="password" class="form-control" placeholder="Password">
+                  </div>
+                  <div class="form-group checkbox">
+                      <input type="checkbox">
+                      <label>Remember me</label>
+                  </div>
+                  <a href="" class="forgot-pass">Forgot Password?</a>
+                  <button class="btn">Login</button>
+                </form>
+                  <a class="sign_up" href="#">sign up</a>
+              </div>
+          </div>
+      </div>
+    </div>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  </body>
+
+</html>
---
+++ rebook-om/request.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>rebook/request</title>
+    <link rel="stylesheet" href="request.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+</head>
+<body>
+    <h1>REQUEST CAMPAIGN</h1>
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $organisationErr = $websiteErr = $phoneErr = "";
+$name = $email = $organisation = $objective = $phone = $website = $book = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL";
+    }
+  }
+
+  if (empty($_POST["objective"])) {
+    $objective = "";
+  } else {
+    $objective = test_input($_POST["objective"]);
+  }
+
+  if (empty($_POST["organisation"])) {
+    $organisationErr = "required";
+  } else {
+    $organisation = test_input($_POST["organisation"]);
+  }
+  if (empty($_POST["book"])) {
+    $book = "";
+  } else {
+    $book = test_input($_POST["book"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+  <p><span class="error">* required field</span></p>
+  <div class="container">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+      <span class="error">* <?php echo $organisationErr;?></span>
+      <input type="radio" id="NGO" name="organisation" value="NGO">
+      <label for="NGO">NGO</label>
+      <input type="radio" id="individual" name="organisation" value="individual">
+      <label for="individual">Individual</label><br>
+          
+      Name<span class="error">* <?php echo $nameErr;?></span>:
+      <input type="text" name="name" value="<?php echo $name;?>">
+      <br><br>
+      
+      E-mail<span class="error">* <?php echo $emailErr;?></span>:
+      <input type="text" name="email" value="<?php echo $email;?>">
+      <br><br>
+      
+      Phone<span class="error">* <?php echo $phoneErr;?></span>:
+      <input type="number" name="phone" value="<?php echo $phone;?>">
+      <br><br>
+
+      Website: <input type="text" name="website" value="<?php echo $website;?>">
+      <span class="error"><?php echo $websiteErr;?></span>
+      <br><br>
+
+      Address: <input type="text" name="address" value="<?php echo $book;?>">
+      <br><br>
+
+      Your Objective: <textarea name="objective" rows="5" cols="40"><?php echo $objective;?></textarea>
+      <br><br>
+
+      Books required: <input type="text" name="book" value="<?php echo $book;?>">
+      <br><br>
+
+      <input type="reset" name="reset" value="reset">
+      
+      <input type="submit" name="submit" value="Submit">  
+    </form>
+  </div>
+</body>
+</html>
