e27369fd4e4deca16e6232a10556eefc0a85910f
Vyasdev <65308856+Vyasdev217@users.noreply.github.com>
2022-02-23 22:47:45 +0530

    Update be_test.php

--- be_test.php
+++ be_test.php
@@ -8,12 +8,12 @@ include "be_initial_setup.php";
 echo "Hello world!\n";
 
 
-echo __toString(db_query("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"));
+echo serialize(db_query("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"));
 
 echo "\n";
 
 initial_setup();
 
-echo __toString(db_query("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"));
+echo serialize(db_query("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';"));
 
 ?>
