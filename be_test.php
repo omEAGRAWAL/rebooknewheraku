eb7263998696b80c9d0a0ff773c86ad6680d2587
Vyasdev <65308856+Vyasdev217@users.noreply.github.com>
2022-02-23 23:40:56 +0530

    Update be_test.php

--- be_test.php
+++ be_test.php
@@ -5,12 +5,12 @@
 include "be.php";
 include "be_initial_setup.php";
 
-print("Hello world!\n");
+print("Hello world!<br>");
 
 
 print(serialize(db_query("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")));
 
-print("\n");
+print("<br>");
 
 initial_setup();
 
