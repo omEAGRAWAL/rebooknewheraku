51c1135733ec16f2f39457f169894aedda5bc523
omEAGRAWAL <95342767+omEAGRAWAL@users.noreply.github.com>
2022-03-07 20:07:19 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -15,85 +15,121 @@
     <title>ReBook</title>
   </head>
 
+
   <body>
-  <header>
-  <div id="header_content" class="header">
-    <nav>
-      <li> <a class="active" href="index.php">
-          <img style="width: 20px;" src="home.png" alt=""></a></li>
-          <li> <a  id="login" style="float:right; cursor:pointer"  type="a" class="login" data-toggle="modal" data-target="#myModal">
-            LogIn
-        </a></li>
-      
-      <li><a class="request" href="request.php">Request campaign</a>
-    </nav>
-  </div>
-  </header>
+<header>
   <div id="header_content" class="header">
     <nav>
       <li> <a class="active" href="index.html">
           <img style="width: 20px;" src="home.png" alt=""></a></li>
       <li><a class="login" id="login" href="login.html" ; target="_parent">LogIn</a></li>
-      <li><a class="request" href="request.php">Request campaign</a>
+      <li><a class="request" href="#book_category">Request campaign</a>
     </nav>
   </div>
-
-  <div class="st1part">
+  </header>
+  
+  <div class="st1part" id="st1part">
     <img id="front1" src="front1.png" alt="">
   </div>
 
-
-  <div id="row1">
-
-<div id="camp1div" class="camp" >
-  <a href="camp1.html"></a>
-  <h2>camp1</h2>
-    <img id="pic1" src="po1.jpg" alt="">
-    <p id="camp1p1" class="image">donate your childs book to them</p>
-      <a class="push_button red" id="button" href="donatebook.html">DONATE BOOK</a>
-    </a>
-  </div>
-
-
-  
+<div id="row1">
 
   <div id="camp1div" class="camp" >
-    <a href="leap.html"></a>
+    <a href="camp1.html"></a>
     <h2>camp1</h2>
-      <img id="pic1" src="po3.jpg" alt="">
-      <p id="camp1p1" class="image">we are helping them to crack jee</p>
-        <a class="push_button red" id="button" href="donatebook.html">DONATE  BOOK</a>
+      <img id="pic1" src="po1.jpg" alt="">
+      <p id="camp1p1" class="image">donate your childs book to them</p>
+        <a class="push_button red" id="button" href="donatebook.html">DONATE BOOK</a>
       </a>
     </div>
-  
-</div>
+
 
     
-    <div id="what_we">
-      <h3 id="what_we_h3">Vison</h3>
-      <p id="what_we line1">To bring all<solid> orphanage</solid> &<br>
-        <solid>NGO</solid>under one plateform</p>
 
+    <div id="camp1div" class="camp" >
+      <a href="leap.html"></a>
+      <h2>camp1</h2>
+        <img id="pic1" src="po3.jpg" alt="">
+        <p id="camp1p1" class="image">we are helping them to crack jee</p>
+          <a class="push_button red" id="button" href="donatebook.html">DONATE  BOOK</a>
+        </a>
+      </div>
+    
+</div>
+
+<div id="our_aim" class="whatudo">
+  <h3 id="our_aimh3">Vison</h3>
+  <p id="what_we line1">To bring all<solid> orphanage</solid> &<br>
+    <solid>NGO</solid>under one plateform</p>
+
+  <p>we are on a mission to help poor childrem with the source of knowledge</p>
+  <p> REUSE of books will reduce the deforastation and carbon emmision</p>
+</div>
+    
+    <div id="whatudo" class="whatudo">
+      <h3 class="h3whatudo">What U Do</h3>
       <p>you will help poor children to get a new bright future</p>
       <p>your one book will be used atleast 3 years</p>
     </div>
 
-
-
-
-    <div id="our_aim" class="our_aim">
-      <p>we are on a mission to help poor childrem with the source of knowledge</p>
-      <p> REUSE of books will reduce the deforastation and carbon emmision</p>
-    </div>
-
-
     <footer>
-    
-      <div id="contact_us" class="contact">
-        <h2>Contact Us</h2>
-        <a href="mailto:dharunramsarathy@gmail.com">Mail address</a>
+
+<!-- Remove the container if you want to extend the Footer to full width. -->
+
+
+  <footer style="background-color: #deded5;">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-6 col-md-12 mb-4">
+        <h5 class="mb-3" style="letter-spacing: 2px; color: #818963;"></h5>
+        <p>
+         We are on a mission help the poor childrens by used books we wnt to build donating book a habit
+        </p>
       </div>
-    </footer> 
+      <div style=" display: flex; flex-direction: row ; align-items: center;  ">
+      <div class="col-lg-3 col-md-6 mb-4" style="   ">
+        <h5 class="mb-3" style="letter-spacing: 2px; color: #818963;">links</h5>
+        <ul class="list-unstyled mb-0">
+          <li class="mb-1">
+            <a href="index.php" style="color: #4f4f4f;">home</a>
+          </li>
+          <li class="mb-1">
+            <a href="camp1.html" style="color: #4f4f4f;">camps</a>
+          </li>
+          <li class="mb-1">
+            <a href="donatebook.php" style="color: #4f4f4f;"> Donate book</a>
+          </li>
+          
+        </ul>
+      </div>
+      <div class="col-lg-3 col-md-6 mb-4">
+        <h5 class="mb-1" style="letter-spacing: 2px; color: #818963;">opening hours</h5>
+        <table class="table" style="color: #4f4f4f; border-color: #666;">
+          <tbody>
+            <tr>
+              <td>adress</td>
+              <td>Nit Puduchery ,karaika,puduchery</td>
+            </tr>
+            <tr>
+              <td>Contact Us</td>
+             <a href="rebookpy@gmail.com"></a> <td>rebookpy@gmail.com/td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Copyright -->
+</footer>
+
+</div>
+<!-- End of .container -->
+
+
+    </footer>
+   
     <?php require 'validate.php' ?>
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
       <div class="modal-dialog" role="document">
