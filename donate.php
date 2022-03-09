4c6c38388bd056939cbd2e9828c8af630ffb3839
Dharun2812 <dharunramsarathy@gmail.com>
2022-03-09 23:20:34 +0530

    changes

--- donate.php
+++ donate.php
@@ -21,8 +21,8 @@
         $fname = $lname = $contact = $email = $add_1 = $add_2 = $city = $district = $state = $pincode = $book_type ="";
         $fnameErr = $emailErr = $contactErr = $add_1Err = $districtErr = $stateErr = $pincodeErr = $book_typeErr = "";
 
-#       $request = array("Fname"=>$fname, "LNAME"=>$lname, "CONTACT"=>$contact, "EMAIL"=>$email, "ADDL1"=>$add_1, "ADDL2"=>$add_2, "CITY"=>$city, 
-#                   "DISTRICT"=>$district, "STATE"=>$state, "PINCODE"=>$pincode, "BOOK_TYPE"=>$book_type);
+#       $request = array("Fname"=>"$fname", "LNAME"=>"$lname", "CONTACT"=>"$contact", "EMAIL"=>"$email", "ADDL1"=>"$add_1", "ADDL2"=>"$add_2", "CITY"=>"$city", 
+#                   "DISTRICT"=>"$district", "STATE"=>"$state", "PINCODE"=>"$pincode", "BOOK_TYPE"=>"$book_type");
 
         if($_SERVER["REQUEST_METHOD"]=="POST"){
             if(empty($_POST["fname"])){
